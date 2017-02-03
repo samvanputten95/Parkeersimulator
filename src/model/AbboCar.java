a6f9ba97bf666754dcc5f271e5360b046e5b6377
samvanputten95 <s.van.putten@st.hanze.nl>
2017-02-03 23:26:50 +0100

    Aangepaste runmethode en werkende knoppen.
    Ik had steeds errors terwijl ik probeerde te pushen, dus ik heb de bestanden op deze manier geüpload.

---
+++ src/model/AbboCar.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.awt.Color;
+import java.util.Random;
+
+public class AbboCar extends Car {
+	private static final Color COLOR=Color.green;
+	
+    public AbboCar() {
+    	Random random = new Random();
+    	int stayMinutes = (int) (15 + random.nextFloat() * 5 * 60);
+        this.setMinutesLeft(stayMinutes);
+        this.setHasToPay(false);
+    }
+    
+    public Color getColor(){
+    	return COLOR;
+    }
+}
