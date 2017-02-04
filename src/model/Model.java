d9cdbf63a3cbf33a32e11a043da79ade663026d3
daveNienhuis <dave.nienhuis@hotmail.com>
2017-02-04 23:42:29 +0100

    Update Model.java
    GUI

--- src/model/Model.java
+++ src/model/Model.java
@@ -3,31 +3,34 @@ import java.awt.*;
 
 import java.util.Random;
 
-import view.MainWindow;
+import view.*;
+import controller.Controller;
+
+import javax.swing.*;
 
 public class Model {
 
-    private static final String AD_HOC = "1";
-    private static final String PASS = "2";
-    private static final String ABBO = "3";
-
-    private CarQueue entranceCarQueue;
+	private static final String AD_HOC = "1";
+	private static final String PASS = "2";
+	private static final String ABBO = "3";
+	
+	private CarQueue entranceCarQueue;
     private CarQueue entrancePassQueue;
     private CarQueue entranceAbboQueue;
     private CarQueue paymentCarQueue;
     private CarQueue exitCarQueue;
     private MainWindow simulatorView;
-
+   
     private boolean run = false;
     private int day = 0;
     private int hour = 0;
     private int minute = 0;
-
+    
     private int aantalCars = 0;
     private int aantalAdHoc = 0;
     private int aantalPass = 0;
     private int aantalAbbo = 0;
-
+    
     private int opbrengst = 0;
 
     private int tickPause = 300;
@@ -42,84 +45,95 @@ public class Model {
     int paymentSpeed = 5; // number of cars that can pay per minute
     int exitSpeed = 3; // number of cars that can leave per minute
 
-
-
+  
+    
     public Model() {
         entranceCarQueue = new CarQueue();
         entrancePassQueue = new CarQueue();
         entranceAbboQueue = new CarQueue();
         paymentCarQueue = new CarQueue();
         exitCarQueue = new CarQueue();
-        simulatorView = new MainWindow(3, 6, 30, this);
+        simulatorView = new MainWindow(3, 6, 30, this);     
     }
-
+    
     public int getOpbrengst() {
         return opbrengst;
     }
-
-
+    
     public int getCars(){
-        return aantalCars;
+    	aantalCars = aantalAdHoc + aantalPass + aantalAbbo;
+    	return aantalCars;
     }
-
+    
     public int getAdHoc(){
-        return aantalAdHoc;
+    	return aantalAdHoc;
     }
-
+    
     public int getPass(){
-        return aantalPass;
+    	return aantalPass;
     }
-
+    
     public int getAbbo(){
-        return aantalAbbo;
+    	return aantalAbbo;
     }
-
+    
+    public int getTime(){
+    	return minute;
+    }
+    
     public void start(){
-
-        run = true;
-        run(10000);
+    	
+    	run = true;
+    	run(10000);
     }
 
     public void run(int stappen) {
         for (int i = 0; i < stappen; i++) {
-            if(run == true){
-                tick();
-            }
-            else{
-                break;
-            }
-        }
-
-    }
-
-    public void pause(){
-        run = false;
-    }
-
-    public void step(){
-
-        for(int i = 0; i < 1; i++){
+        	if(run == true){
             tick();
+        	}
+        	else{
+        		break;
+        	}
         }
+    	
     }
-
+    
+    public void pause(){
+    	run = false;
+    }
+    
+    public void step(){
+    	
+    	for(int i = 0; i < 1; i++){
+    		tick();
+    	}
+    }
+    
+    public void uur(){
+    	
+    	for(int i = 0; i < 60; i++){
+    		tick();
+    	}
+    }
+    
     public void quit(){
-        System.exit(0);
+    	System.exit(0);
     }
 
     private void tick() {
-        advanceTime();
-        handleExit();
-        updateViews();
-        // Pause.
+    	advanceTime();
+    	handleExit();
+    	updateViews();
+    	// Pause.
         try {
             Thread.sleep(tickPause);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        handleEntrance();
+    	handleEntrance();
     }
-
+        
     private void advanceTime(){
         // Advance the time by one minute.
         minute++;
@@ -136,30 +150,30 @@ public class Model {
         }
 
     }
-
+    
     private void handleEntrance(){
-        carsArriving();
-        carsEntering(entranceAbboQueue);
-        carsEntering(entrancePassQueue);
-        carsEntering(entranceCarQueue);
+    	carsArriving();
+    	carsEntering(entranceAbboQueue);
+    	carsEntering(entrancePassQueue);
+    	carsEntering(entranceCarQueue);  	
     }
-
+    
     private void handleExit(){
         carsReadyToLeave();
         carsPaying();
         carsLeaving();
     }
-
+    
     private void updateViews(){
-        simulatorView.tick();
+    	simulatorView.tick();
         // Update the car park view.
-        simulatorView.updateView();
+        simulatorView.updateView();	
     }
-
+    
     private void carsArriving(){
-        int numberOfCars=getNumberOfCars(weekDayArrivals, weekendArrivals);
-        addArrivingCars(numberOfCars, AD_HOC);
-        numberOfCars=getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
+    	int numberOfCars=getNumberOfCars(weekDayArrivals, weekendArrivals);
+        addArrivingCars(numberOfCars, AD_HOC);    	
+    	numberOfCars=getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
         addArrivingCars(numberOfCars, PASS);
         numberOfCars=getNumberOfCars(weekDayAbboArrivals, weekendAbboArrivals);
         addArrivingCars(numberOfCars, ABBO);
@@ -167,49 +181,42 @@ public class Model {
 
     private void carsEntering(CarQueue queue){
         int i=0;
-        // Remove car from the front of the queue and assign to a parking space.
-        while (queue.carsInQueue()>0 &&
-                simulatorView.getNumberOfOpenSpots()>0 &&
-                i<enterSpeed) {
+    	while (queue.carsInQueue()>0 && 
+    			simulatorView.getNumberOfOpenSpots()>0 && 
+    			i<enterSpeed) {
             Car car = queue.removeCar();
-
+            
             if(car.getColor() == Color.red){
-                Location vrijePlek = simulatorView.getFirstFreeLocation();
-                simulatorView.setCarAt(vrijePlek, car);
-                aantalCars++;
-                aantalAdHoc++;
+            	Location vrijePlek = simulatorView.getFirstFreeLocation();
+            	simulatorView.setCarAt(vrijePlek, car);
+            	aantalAdHoc++;
             }
-
+            
             if(car.getColor() == Color.blue){
-                Location vrijePlek = simulatorView.getFirstFreeLocation();
-                simulatorView.setCarAt(vrijePlek, car);
-                aantalCars++;
-                aantalPass++;
+            	Location vrijePlek = simulatorView.getFirstFreeLocation();
+            	simulatorView.setCarAt(vrijePlek, car);
+            	aantalPass++;
             }
-
+            
             if(car.getColor() == Color.green){
-                Location vrijePlek = simulatorView.getFirstFreeLocation();
-                simulatorView.setCarAt(vrijePlek, car);
-                aantalCars++;
-                aantalAbbo++;
+            	Location vrijePlek = simulatorView.getFirstFreeLocation();
+            	simulatorView.setCarAt(vrijePlek, car);
+            	aantalAbbo++;
             }
-            // Location location = simulatorView.getFirstFreeLocation();
-            //simulatorView.setCarAt(freeLocation, car);
-            //i++;
         }
-    }
-
+     }
+    
     private void carsReadyToLeave(){
         // Add leaving cars to the payment queue.
         Car car = simulatorView.getFirstLeavingCar();
         while (car!=null) {
-            if (car.getHasToPay()){
-                car.setIsPaying(true);
-                paymentCarQueue.addCar(car);
-            }
-            else {
-                carLeavesSpot(car);
-            }
+        	if (car.getHasToPay()){
+	            car.setIsPaying(true);
+	            paymentCarQueue.addCar(car);
+        	}
+        	else {
+        		carLeavesSpot(car);
+        	}
             car = simulatorView.getFirstLeavingCar();
         }
     }
@@ -231,17 +238,17 @@ public class Model {
             i++;
         }
     }
-
+    
     private void carsLeaving(){
         // Let cars leave.
-        int i=0;
-        while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
+    	int i=0;
+    	while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
             exitCarQueue.removeCar();
             i++;
             aantalCars--;
-        }
+    	}	
     }
-
+    
     private int getNumberOfCars(int weekDay, int weekend){
         Random random = new Random();
 
@@ -253,34 +260,34 @@ public class Model {
         // Calculate the number of cars that arrive this minute.
         double standardDeviation = averageNumberOfCarsPerHour * 0.3;
         double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
-        return (int)Math.round(numberOfCarsPerHour / 60);
+        return (int)Math.round(numberOfCarsPerHour / 60);	
     }
-
+    
     private void addArrivingCars(int numberOfCars, String type){
         // Add the cars to the back of the queue.
-        switch(type) {
-            case AD_HOC:
-                for (int i = 0; i < numberOfCars; i++) {
-                    entranceCarQueue.addCar(new AdHocCar());
-                }
-                break;
-            case PASS:
-                for (int i = 0; i < numberOfCars; i++) {
-                    entrancePassQueue.addCar(new ParkingPassCar());
-                }
-                break;
-            case ABBO:
-                for (int i = 0; i < numberOfCars; i++) {
-                    entranceAbboQueue.addCar(new AbboCar());
-                }
-                break;
-        }
+    	switch(type) {
+    	case AD_HOC: 
+            for (int i = 0; i < numberOfCars; i++) {
+            	entranceCarQueue.addCar(new AdHocCar());
+            }
+            break;
+    	case PASS:
+            for (int i = 0; i < numberOfCars; i++) {
+            	entrancePassQueue.addCar(new ParkingPassCar());
+            }
+            break;	  
+    	case ABBO:
+            for (int i = 0; i < numberOfCars; i++) {
+            	entranceAbboQueue.addCar(new AbboCar());
+            }
+            break;	
+    	}
     }
-
+    
     private void carLeavesSpot(Car car){
-        simulatorView.removeCarAt(car.getLocation());
+    	simulatorView.removeCarAt(car.getLocation());
         exitCarQueue.addCar(car);
     }
-
-
+    
+    
 }
