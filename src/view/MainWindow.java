61ab790bcce8e52a076e621e831aab337d11bda8
daveNienhuis <dave.nienhuis@hotmail.com>
2017-02-04 20:59:21 +0100

    Update MainWindow.java
    Total opbrengst geadd

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -18,154 +18,174 @@ import javax.swing.JPanel;
  */
 
 public class MainWindow implements ActionListener {
-	//extends JFrame
-	
-	private CarParkView carParkView;
+    //extends JFrame
+
+    private CarParkView carParkView;
     private int numberOfFloors;
     private int numberOfRows;
     private int numberOfPlaces;
     private int numberOfOpenSpots;
-    private JFrame mainwindow; 
+    private JFrame mainwindow;
     private Car[][][] cars;
     private Model model;
     private ActionEvent event;
-    
+
     private JPanel buttonbar;
     private JPanel infobar;
+    private JPanel geldbar;
+
     private JLabel Cars;
     private JLabel AdHoc;
     private JLabel Pass;
     private JLabel Abbo;
 
+    private JLabel opbrengst;
+
     public MainWindow(int numberOfFloors, int numberOfRows, int numberOfPlaces, Model model) {
-    	mainwindow=new JFrame("GarageSimulator");
-    	
+        mainwindow=new JFrame("GarageSimulator");
+
         this.numberOfFloors = numberOfFloors;
         this.numberOfRows = numberOfRows;
         this.numberOfPlaces = numberOfPlaces;
-        this.numberOfOpenSpots =numberOfFloors*numberOfRows*numberOfPlaces;
-        
+        this.numberOfOpenSpots = numberOfFloors*numberOfRows*numberOfPlaces;
+
         this.model = model;
         Controller controller = new Controller(model);
-        
+
         cars = new Car[numberOfFloors][numberOfRows][numberOfPlaces];
-        
+
         carParkView = new CarParkView(this);
 
-        
-        
+
+
         this.buttonbar = new JPanel();
         buttonbar.setLayout(new GridLayout(1, 0));
+
         this.infobar = new JPanel();
         infobar.setLayout(new GridLayout(1,0));
-		mainwindow.getContentPane().add(buttonbar);
+
+        this.geldbar = new JPanel();
+        geldbar.setLayout(new GridLayout(1, 0));
+
+        mainwindow.getContentPane().add(buttonbar);
         mainwindow.getContentPane().add(infobar);
-        
-        
+        mainwindow.getContentPane().add(geldbar);
+
+
         Container contentPane = mainwindow.getContentPane();
-        
-       
+
+
         JButton start = new JButton("start");
         start.addActionListener(this);
-        
+
         JButton step = new JButton("step");
         step.addActionListener(this);
-        
+
         JButton pause = new JButton("pause");
         pause.addActionListener(this);
-        
+
         JButton quit = new JButton("quit");
         quit.addActionListener(this);
-        
+
         buttonbar.add(start);
         buttonbar.add(step);
         buttonbar.add(pause);
         buttonbar.add(quit);
-        
+
         Cars = new JLabel("Totaalaantal Cars");
         Cars.setHorizontalAlignment(SwingConstants.CENTER);
         Cars.setText(String.valueOf(model.getCars()));
-        
+
         AdHoc = new JLabel("aantal AdHocCars");
         AdHoc.setHorizontalAlignment(SwingConstants.CENTER);
         AdHoc.setText(String.valueOf(model.getAdHoc()));
-        
+
         Pass = new JLabel("Aantal ParkingPassCars");
         Pass.setHorizontalAlignment(SwingConstants.CENTER);
         Pass.setText(String.valueOf(model.getPass()));
-        
+
         Abbo = new JLabel("Aantal AbboCars");
         Abbo.setHorizontalAlignment(SwingConstants.CENTER);
         Abbo.setText(String.valueOf(model.getAbbo()));
-        
+
+        opbrengst = new JLabel("Totaal Opbrengst");
+        opbrengst.setHorizontalAlignment(SwingConstants.CENTER);
+        opbrengst.setText(String.valueOf(model.getOpbrengst()));
+
         infobar.add(Cars);
         infobar.add(AdHoc);
         infobar.add(Pass);
         infobar.add(Abbo);
-        
-        
-        JPanel flow = new JPanel(new GridLayout(0,1));       
+
+        geldbar.add(opbrengst);
+
+
+        JPanel flow = new JPanel(new GridLayout(0,1));
         flow.add(buttonbar);
         flow.add(infobar);
+        flow.add(geldbar);
         contentPane.add(carParkView, BorderLayout.NORTH);
         contentPane.add(flow, BorderLayout.SOUTH);
-        
+
         mainwindow.pack();
         mainwindow.setVisible(true);
 
         updateView();
     }
- 
-   
-    
+
+
+
     public void setActionEvent(ActionEvent e) {
         event = e;
     }
-    
+
     public ActionEvent getActionEvent() {
         return event;
     }
-    
+
     public void actionPerformed(ActionEvent e) {
         setActionEvent(e);
-        
+
         Thread newThread = new Thread() {
             public void run() {
-            	
-                ActionEvent event = getActionEvent();                
+
+                ActionEvent event = getActionEvent();
                 String command = event.getActionCommand();
-                
+
                 if (command == "start") {
-                	model.start();                    
+                    model.start();
                 }
-                
+
                 if(command == "pause") {
-                	model.pause();                    
+                    model.pause();
                 }
-                
+
                 if(command == "step") {
-                	
-                	model.step();                    
+
+                    model.step();
                 }
-                                                   
+
                 if (command == "quit") {
-                	model.quit();                                        
-                }                
-            }          
-        };        
-        newThread.start();    
+                    model.quit();
+                }
+            }
+        };
+        newThread.start();
     }
-    
+
     public void updateView() {
         carParkView.updateView();
-        
+
+
         Cars.setText("Totaalaantal Cars: " + String.valueOf(model.getCars()));
         AdHoc.setText("Aantal AdHocCars: " + String.valueOf(model.getAdHoc()));
         Pass.setText("Aantal ParkingPassCars: " + String.valueOf(model.getPass()));
         Abbo.setText("Aantal AbboCars: " + String.valueOf(model.getAbbo()));
+        opbrengst.setText("Totaal Opbrengst: € " + String.valueOf(model.getOpbrengst())+ ",-");
+
     }
-    
-	public int getNumberOfFloors() {
+
+    public int getNumberOfFloors() {
         return numberOfFloors;
     }
 
@@ -178,9 +198,9 @@ public class MainWindow implements ActionListener {
     }
 
     public int getNumberOfOpenSpots(){
-    	return numberOfOpenSpots;
+        return numberOfOpenSpots;
     }
-    
+
     public Car getCarAt(Location location) {
         if (!locationIsValid(location)) {
             return null;
@@ -268,5 +288,5 @@ public class MainWindow implements ActionListener {
         }
         return true;
     }
-    
+
 }
