c8f0c473edfe005143772483fe7d7e31978275e5
nielswildeboer <niels.wildeboer@hotmail.nl>
2017-02-05 13:44:06 +0100

    Update CarQueue.java
    Javadoc

--- src/model/CarQueue.java
+++ src/model/CarQueue.java
@@ -1,19 +1,34 @@
 package model;
+
+/*
+ * Author Par-Kings
+ * Version 2017.1.25
+ */
+
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class CarQueue {
     private Queue<Car> queue = new LinkedList<>();
-
+/* 
+* @return queue.add(car)
+*/
     public boolean addCar(Car car) {
         return queue.add(car);
     }
-
+/* 
+* @return queue.add(car)
+*/
     public Car removeCar() {
         return queue.poll();
     }
-
+/* 
+* @return queue.poll()
+*/
     public int carsInQueue(){
     	return queue.size();
     }
+/* 
+* @return queue.size()
+*/
 }
