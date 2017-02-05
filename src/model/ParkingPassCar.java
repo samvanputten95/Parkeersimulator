a4ef399ea89be02a8cfaf3e9b311277b40c1c424
zerin6 <z.sulejmanovic@st.hanze.nl>
2017-02-05 13:28:29 +0100

    Java doc toegevoegd

--- src/model/ParkingPassCar.java
+++ src/model/ParkingPassCar.java
@@ -3,8 +3,25 @@ package model;
 import java.util.Random;
 import java.awt.*;
 
+/**
+ * Dit is de klasse ParkingPassCar
+ * 
+ * @author Zerin
+ */
+
+/**
+ * 
+ * Deze methode geeft de ParkinPassCar auto's een blauwe kleur.
+ *
+ */
 public class ParkingPassCar extends Car {
 	private static final Color COLOR=Color.blue;
+	
+	/**
+	 * 
+	 * Randomiseert de duur van het verblijf van de auto en geeft aan dat er niet betaald moet worden.
+	 *
+	 */
 	
     public ParkingPassCar() {
     	Random random = new Random();
@@ -13,6 +30,10 @@ public class ParkingPassCar extends Car {
         this.setHasToPay(false);
     }
     
+    /**
+     * 
+     * @return retourneert de kleur.
+     */
     public Color getColor(){
     	return COLOR;
     }
