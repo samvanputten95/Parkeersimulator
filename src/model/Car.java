f6d29788f8528e70b671b52073a3718e46480c14
TheMoonBunny <themoonbunnys@gmail.com>
2017-02-05 13:57:42 +0100

    Update Car.java
    javadoc toegevoegd

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,6 +1,10 @@
 package model;
 
 import java.awt.*;
+/*
+ * 
+ *  dit is Car.java
+ */
 
 public abstract class Car {
 
@@ -10,47 +14,72 @@ public abstract class Car {
     private boolean hasToPay;
 
     /**
-     * Constructor for objects of class Car
+     * Constructor voor object class Car
      */
     public Car() {
 
     }
-
+    /*
+     *  laat zien de locaties van de auto
+     */
     public Location getLocation() {
         return location;
     }
-
+    	/*
+    	 * geef een locatie aan een auto
+    	 */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+	/*
+	 *  returneert hoeveel minuten er nog overbleeft
+	 */
     public int getMinutesLeft() {
         return minutesLeft;
     }
+    /*
+     * zet hoeveel minuten er nog over is
+     */
 
     public void setMinutesLeft(int minutesLeft) {
         this.minutesLeft = minutesLeft;
     }
-    
+
+    /*
+     * returneert ja of nee
+     */
     public boolean getIsPaying() {
         return isPaying;
     }
+    /*
+     *  het is een boolean die kijkt of hij betaalt ja of nee
+     */
 
     public void setIsPaying(boolean isPaying) {
         this.isPaying = isPaying;
     }
-
+    /*
+     * returneert hoeveel betalt moet worden
+     */
     public boolean getHasToPay() {
         return hasToPay;
     }
-
+    	/*
+    	 * zet hoeveel er betaald moet worden
+    	 */
     public void setHasToPay(boolean hasToPay) {
         this.hasToPay = hasToPay;
     }
-
+    /*
+     *  elke keer als het geroept wordt haalt het -1 
+     */
     public void tick() {
         minutesLeft--;
     }
     
+    /*
+     *  haalt de kleur op
+     */
     public abstract Color getColor();
 }
